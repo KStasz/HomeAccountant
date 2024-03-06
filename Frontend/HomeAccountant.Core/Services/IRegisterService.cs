bae1ea86e5cd7dd792d328fff52acb2f38b351c9
KStasz <61710462+KStasz@users.noreply.github.com>
2024-03-06 09:52:33 +0100

    Problem fixed

--- Frontend/HomeAccountant.Core/Services/IRegisterService.cs
+++ Frontend/HomeAccountant.Core/Services/IRegisterService.cs
@@ -1,10 +1,10 @@
-﻿using HomeAccountant.Core.DTOs.Register;
-using HomeAccountant.Core.Model;
+﻿using HomeAccountant.Core.Model;
 
 namespace HomeAccountant.Core.Services
 {
     public interface IRegisterService
     {
+        Task<ServiceResponse<RegisterModel?>> GetRegister(int registerId, CancellationToken cancellationToken = default);
         Task<ServiceResponse<IEnumerable<RegisterModel>?>> GetRegistersAsync(CancellationToken cancellationToken = default);
         Task<ServiceResponse> CreateRegisterAsync(RegisterModel registerCreateDto, CancellationToken cancellationToken = default);
         Task<ServiceResponse> DeleteRegisterAsync(int registerId, CancellationToken cancellationToken = default);
